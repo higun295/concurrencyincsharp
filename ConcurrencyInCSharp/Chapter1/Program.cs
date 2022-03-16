2c1495cabfad3585a15190eafd46f5ac262e80ed
HEEKWON <higun295@gmail.com>
2022-03-16 09:04:44 +0900

    20220316_1

---
+++ ConcurrencyInCSharp/Chapter1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Chapter1 {
+    class Program {
+        static void Main(string[] args) {
+            Task task = DoSomethingAsync();
+            task.Wait();
+        }
+
+        static async Task DoSomethingAsync() {
+            int val = 13;
+            Console.WriteLine(val);
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            val *= 2;
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            Console.WriteLine(val);
+        }
+    }
+}
